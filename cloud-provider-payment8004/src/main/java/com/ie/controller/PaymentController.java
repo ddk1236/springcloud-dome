a93a42257e87a917e066592baf6b86ee970e3dd4
代登科 <1571905344@qq.com>
2021-10-25 17:57:33 +0800

    zookeeper使用测试

---
+++ cloud-provider-payment8004/src/main/java/com/ie/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.ie.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author ddk
+ * @date 2021/10/25
+ */
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/zk")
+    public String testZookeeper(){
+        return "springCloud with zookeeper:" +serverPort +"\t"+ UUID.randomUUID().toString();
+    }
+}
